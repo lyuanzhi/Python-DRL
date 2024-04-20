48cec09919e989a09955ff66aa7ba219a6ac2549
Yuanzhi Lou <79785497+lyuanzhi@users.noreply.github.com>
2024-04-20 04:07:46 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Python-DRL',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     description='A pytorch-based deep reinforcement learning package',
     long_description=open('README.md').read(),
