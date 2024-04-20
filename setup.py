330e7ddc3ff8c0b40d861fd8826acdbf58ea0a44
lyuanzhi <lyuanzhi862066195@outlook.com>
2024-04-20 01:28:07 -0400

    build package structure

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Python-DRL',
+    version='1.0.0',
+    packages=find_packages(),
+    description='A pytorch-based deep reinforcement learning package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='lyuanzhi',
+    author_email='lyuanzhi862066195@outlook.com',
+    url='https://github.com/lyuanzhi/Python-DRL',
+    install_requires=[
+        'numpy>=1.24.0',
+        'torch>=2.0.1',
+        'gym~=0.26.2',
+        'opencv-python>=4.5.5.62'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
+
